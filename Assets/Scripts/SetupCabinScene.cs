15688a8015f3cce913b089ac7e1fc96d0599eb81
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-02-16 17:15:19 -0600

    Revert "Restroom additions and minor adjustments"
    This reverts commit e6e3c9ab402ec6d2ae4cd7964ef415bb4c212a1e.

--- Assets/Scripts/SetupCabinScene.cs
+++ Assets/Scripts/SetupCabinScene.cs
@@ -7,23 +7,10 @@ public class SetupCabinScene : MonoBehaviour
 {
 
     private GameObject player;
-    private bool insideCabin;
 
     void Start()
     {
         player = GameObject.Find("First Person Player");
-        insideCabin = false;
-    }
-
-    void Update()
-    {
-        if(player.transform.position.x > 515 || player.transform.position.x < 480 || player.transform.position.z > 510 || player.transform.position.z < 480)
-        insideCabin = false;
-
-        if(player != null && !insideCabin)
-        {
-            player.transform.position = new Vector3(500, 4.1f, 500);
-            insideCabin = true;
-        }
+        player.transform.position = new Vector3(500, 6, 500);
     }
 }
